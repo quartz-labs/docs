58019cb1af28c6755c48f7ed491a9792ca25cd11
iarla-crewe <iarla@quartzpay.io>
2024-12-18 13:00:40 +0000

    Update open-graph image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -47,7 +47,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/social-card.jpg',
+    image: 'https://cdn.prod.website-files.com/67504dd7fde047775f88c355/67504dd7fde047775f88c404_open-graph.jpg',
     navbar: {
       title: 'Quartz',
       logo: {
